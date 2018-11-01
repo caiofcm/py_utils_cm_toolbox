388ce558a0794e3e49430c442cba246d132d4206
Caio Marcellos <caiocuritiba@gmail.com>
2018-11-01 11:03:06 -0300

    enhancement: included gbn signal generation

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,12 +1,21 @@
 import numpy as np
 from utils_cm_toolbox import utils_signal
+from matplotlib import pyplot as plt
+
+PLOT = True
 
 def test_generate_gbn():
-    low = np.array([1.0])
-    upp = np.array([5.0])
-    tspan = np.linspace(0.0, 10, 11)
-    prob_change = np.array([0.95])
-    num_samples_min = 2
+    low = np.array([1.0, -10.0])
+    upp = np.array([5.0, 2.0])
+    tspan = np.linspace(0.0, 10, 101)
+    prob_change = np.array([0.95, 0.95])
+    num_samples_min = [2, 10]
     args = (tspan, low, upp, prob_change, num_samples_min)
-    utils_signal.generate_gbn(*args)
-    assert(True)
+    out_signal = utils_signal.generate_gbn(*args)
+    if PLOT:
+        plt.figure()
+        plt.plot(tspan, out_signal, '.-k', lw=2, label='signal')
+        plt.legend()
+        plt.show()
+    assert(out_signal[0, 0] == low[0])
+    assert(out_signal[0, 1] == low[1])
--- utils_cm_toolbox/utils_signal.py
+++ utils_cm_toolbox/utils_signal.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-def generate_gbn(tspan, lower_vals, upper_vals, prob_change, number_of_samples_min):
+def generate_gbn(tspan: np.ndarray, 
+        lower_vals: np.ndarray,
+        upper_vals: np.ndarray,
+        prob_change: np.ndarray,
+        number_of_samples_min: (list, np.ndarray)) -> np.ndarray:
     previous_signal = lower_vals.copy()
     n = len(previous_signal)
     last_t_change = np.zeros(n)
     nt = len(tspan)
+    dt = tspan[1] - tspan[0]
     signal = np.zeros((nt, n))
+    np.random.seed()
     for k in range(nt):
         actual_time = tspan[k]
         signal_aux = np.zeros(n)
         R = np.random.uniform(size=n)
         for i in range(n):
             if R[i] < prob_change[i]:
-                if (actual_time - last_t_change[i]) < number_of_samples_min:
+                if int((actual_time - last_t_change[i])/dt) < number_of_samples_min[i]:
                     if previous_signal[i] == upper_vals[i]:
                         signal_aux[i] = upper_vals[i]
                     else:

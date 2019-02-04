db75ccfb241f52c687f81418dc7bf214a465df95
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2019-02-04 12:46:07 -0200

    b-splines added

---
+++ tests/test_numericals.py
@@ -0,0 +1,59 @@
+import pytest
+import numpy as np
+from utils_cm_toolbox import numericals
+import matplotlib.pyplot as plt
+
+NUM_POINTS_REF = 10
+T_REF = np.linspace(0., 10., NUM_POINTS_REF + 1)
+Y_REF = np.array([10., 9., 11., 10., 5., 4., 7., 8., 5., 9., 11.])
+
+# def test_b_spline_coeff():
+#     coefs, h = numericals.b_spline_coeff(T_REF, Y_REF)
+
+#     assert coefs[0] == 5.0
+
+# def test_b_spline_eval():
+#     coeffs_test = np.ones(NUM_POINTS_REF + 1)
+#     coeffs_test[0::2] = -1
+#     x = 4.5
+#     r = numericals.b_spline_eval(T_REF, coeffs_test, x)
+
+    # assert r == 5.0
+
+def test_b_spline():
+    coefs, h = numericals.b_spline_coeff(T_REF, Y_REF)
+    x = 4.5
+    r = numericals.b_spline_eval(T_REF, coefs, h, x)
+    assert r < 5.0 and r > 4.0
+
+
+def show_plot_b_spline():
+    coefs, h = numericals.b_spline_coeff(T_REF, Y_REF)
+    nt = 101
+    x = np.linspace(0.0, T_REF[-1], nt)
+    r = np.empty_like(x)
+    for i in np.arange(0, nt):
+        r[i] = numericals.b_spline_eval(T_REF, coefs, h, x[i])
+    plt.figure()
+    plt.plot(x, r, 'o-')
+    plt.plot(T_REF, Y_REF, 'dr')
+    print(coefs)
+    plt.show()
+
+def case_testing_b_spline():
+    coefs, h = numericals.b_spline_coeff(T_REF, Y_REF)
+    coefs = np.random.random(11) * len(coefs)
+    nt = 101
+    x = np.linspace(0.0, T_REF[-1], nt)
+    r = np.empty_like(x)
+    for i in np.arange(0, nt):
+        r[i] = numericals.b_spline_eval(T_REF, coefs, h, x[i])
+    plt.figure()
+    plt.plot(x, r, 'o-')
+    plt.plot(T_REF, Y_REF, 'dr')
+    print(coefs)
+    plt.show()    
+
+if __name__ == "__main__":
+    # case_testing_b_spline()
+    show_plot_b_spline()
--- utils_cm_toolbox/numericals.py
+++ utils_cm_toolbox/numericals.py
@@ -61,6 +61,17 @@ def regularization_mult_func(x, A0, Aspan, xEspan, psi):
         Apast = Aspan[i]
     return reg
 
+import matplotlib.pyplot as plt
+def eval_regularization():
+    plt.figure()
+    xspan = np.linspace(0, 200, 501)
+    Ai = [10.0, 5.0]
+    xEi = [25.0, 100.0]
+    psi = [0.5, 0.05]
+    y = regularization_mult_func(xspan, 0.0, Ai, xEi, psi)
+    plt.plot(xspan, y)
+    plt.title('Multiple step reg function for Aggregation Kernel')
+    plt.show()    
 # def jit_regularization_funcs():
 #     # global regularization_func, regularization_mult_func
 #     regularization_func = numba.njit(regularization_func)
@@ -103,17 +114,104 @@ def step_rk3lmMOD(t0, h, y, fun, rhs, arg = None):
 
     return y
 
-import matplotlib.pyplot as plt
-def eval_regularization():
-    plt.figure()
-    xspan = np.linspace(0, 200, 501)
-    Ai = [10.0, 5.0]
-    xEi = [25.0, 100.0]
-    psi = [0.5, 0.05]
-    y = regularization_mult_func(xspan, 0.0, Ai, xEi, psi)
-    plt.plot(xspan, y)
-    plt.title('Multiple step reg function for Aggregation Kernel')
-    plt.show()    
+
+############################################
+###########################################
+#
+#	 B-SPLINE IMPLEMENTATION
+#Ref: http://www.lce.hut.fi/teaching/S-114.1100/lect_6.pdf p 29
+###########################################
+############################################
+
+@numba.njit()
+def b_spline_coeff(t: np.ndarray, y: np.ndarray):
+    """Calculates b-spline coefficients
+    
+    Parameters
+    ----------
+    t : np.ndarray [n+1]
+        independent variable points
+    y : np.ndarray [n+1]
+        dependent variable points
+    
+    Returns
+    -------
+    tuple(np.ndarray, np.ndarray)
+        [0]: Coefficients array [n+2]
+        [1]: Adjusted interval array [n+2]
+    """
+    n = len(t) - 1 #n: number of intervals
+    h = b_spline_intervals(t)
+
+    # Determine A0:
+    delta = -1.0
+    gamma = 2.0 * y[0]
+    p = delta*gamma
+    q = 2.0
+    # r = h[2:]/h[]
+
+    for i in np.arange(1, n + 1):
+        r = h[i+1]/h[i]
+        delta = -r*delta
+        gamma = -r*gamma + (r+1.0)*y[i]
+        p = p + gamma*delta
+        q = q + delta*delta
+    
+    a = np.empty(n + 2)
+    a[0]=-p/q
+    # Determine other coefficients Ai:
+    for i in np.arange(1, n + 2):
+        a[i] = ((h[i-1]+h[i])*y[i-1]-h[i]*a[i-1])/h[i-1]
+    
+    return a, h
+
+@numba.njit()
+def b_spline_intervals(t):
+    n = len(t) - 1 #n: number of intervals
+    h = np.empty(n+2)
+    h[1:-1] = t[1:] - t[0:-1]
+    h[0] = h[1]
+    h[-1] = h[-2]
+    return h
+
+@numba.njit()
+def b_spline_eval(t: np.ndarray, a: np.ndarray, h: np.ndarray, x: float):
+    """Evaluate the b-spline
+    
+    Parameters
+    ----------
+    t : np.ndarray
+        independent points 
+    a : np.ndarray
+        b-spline coefficients
+    h : np.ndarray
+        modified interval array
+    x : float
+        defined point to interpolate
+    
+    Returns
+    -------
+    float, nd.array
+        Interpolated value (values)
+    """
+
+    # Check in which interval x lies
+    n = len(t) - 1
+    for i in np.arange(n - 1, 0 - 1, -1):
+        if (x - t[i] >= 0.):
+            break
+
+    # /* Evaluate S(x) */
+    # h = t[1:] - t[0:-1]
+
+    i = i+1
+    d = (a[i+1]*(x-t[i-1])+a[i]*(t[i]-x+h[i+1]))/(h[i]+h[i+1])
+    e = (a[i]*(x-t[i-1]+h[i-1])+a[i-1]*(t[i-1]-x+h[i]))/(h[i-1]+h[i])
+    result = (d*(x-t[i-1])+e*(t[i]-x))/h[i]
+
+    return result
+
+
 
 if __name__ == "__main__":
     eval_regularization()

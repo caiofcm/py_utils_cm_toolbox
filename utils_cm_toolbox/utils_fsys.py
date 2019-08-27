364bf5a509cfaa6ad6e1c52334ae58e4541f9781
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2019-08-27 09:41:02 -0300

    sync

--- utils_cm_toolbox/utils_fsys.py
+++ utils_cm_toolbox/utils_fsys.py
@@ -15,3 +15,12 @@ def getFileDir():
 	return os.path.dirname(os.path.abspath(__file__))
 
 # def #CREATE RELATIVE IMPORT FUNCTION TODO
+
+def get_relative_dir(rel_path):
+    FILE_DIR = os.path.dirname('__file__')
+    rel = os.path.abspath(os.path.join(FILE_DIR, '../'))
+    return rel
+def relative_import(mod_name, rel_path):
+    rel = get_relative_dir(rel_path)
+    sys.path.append(rel)
+    return importlib.import_module(mod_name)

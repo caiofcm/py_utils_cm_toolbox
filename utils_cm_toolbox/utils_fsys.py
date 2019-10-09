e7d99c2bbab87d63c7b255a01bd5d9e6dc204b1e
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2019-10-09 17:01:33 -0300

    fixed relative import

--- utils_cm_toolbox/utils_fsys.py
+++ utils_cm_toolbox/utils_fsys.py
@@ -19,15 +19,27 @@ def getFileDir():
 
 # def #CREATE RELATIVE IMPORT FUNCTION TODO
 
+# def get_relative_dir(rel_path):
+#     FILE_DIR = os.path.dirname('__file__')
+#     rel = os.path.abspath(os.path.join(FILE_DIR, '../'))
+#     return rel
+# def relative_import(mod_name, rel_path):
+#     rel = get_relative_dir(rel_path)
+#     sys.path.append(rel)
+#     return importlib.import_module(mod_name)
+
 def get_relative_dir(rel_path):
-    FILE_DIR = os.path.dirname('__file__')
-    rel = os.path.abspath(os.path.join(FILE_DIR, '../'))
+    try:
+        FILE_DIR = os.path.dirname('__file__')
+    except:
+        FILE_DIR = os.path.dirname(__file__)
+    rel = os.path.abspath(os.path.join(FILE_DIR, rel_path))
     return rel
 def relative_import(mod_name, rel_path):
     rel = get_relative_dir(rel_path)
-    sys.path.append(rel)
+    if rel not in sys.path:
+        sys.path.append(rel)
     return importlib.import_module(mod_name)
-
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj): #pylint: disable=method-hidden

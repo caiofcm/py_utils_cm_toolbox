adcfffb5e1ecb45342e8d4e752fc9a3e5bc29e1c
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2020-10-05 09:22:15 -0300

    adding git root

--- utils_cm_toolbox/utils_fsys.py
+++ utils_cm_toolbox/utils_fsys.py
@@ -3,6 +3,8 @@ import sys
 import importlib
 import json
 import numpy as np
+from functools import lru_cache
+
 ############################################
 ###########################################
 #
@@ -33,8 +35,11 @@ def get_relative_dir(rel_path):
         FILE_DIR = os.path.dirname('__file__')
     except:
         FILE_DIR = os.path.dirname(__file__)
+    if FILE_DIR == '':
+        FILE_DIR = os.path.dirname(__file__)
     rel = os.path.abspath(os.path.join(FILE_DIR, rel_path))
     return rel
+
 def relative_import(mod_name, rel_path):
     rel = get_relative_dir(rel_path)
     if rel not in sys.path:
@@ -46,3 +51,15 @@ class NumpyEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)    
+
+
+@lru_cache(maxsize=1)
+def git_root():
+    from subprocess import check_output, CalledProcessError
+    ''' returns the absolute path of the repository root '''
+    try:
+        base = check_output('git rev-parse --show-toplevel', shell=True)
+    except CalledProcessError:
+        raise IOError('Current working directory is not a git repository')
+    return base.decode('utf-8').strip()
+# ROOT_DIR = git_root()        
